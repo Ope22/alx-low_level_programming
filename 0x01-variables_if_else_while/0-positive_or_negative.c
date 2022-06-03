555f41585a454635be31ffab975f026d93ac1377
Ope22 <opemipoi@outlook.com>
2022-06-03 16:38:15 -0700

    positive/negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		int n, last;
+
+		srand(time(0));
+		n = rand() - RAND_MAX / 2;
+		last = n % 10;
+		if (last > 5)
+		{
+			printf("%d is positive\n", n);
+		}
+		else if (last < 6 && last != 0)
+		{
+			printf("%d is negative\n", n);
+		}
+		else
+		{
+			printf("%d is zerp\n", n);
+		}
+		return (0);
+}
