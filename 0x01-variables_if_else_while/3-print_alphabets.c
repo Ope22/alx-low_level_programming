ab4b6aae4458ccd12c7f3e14f073d12b03359a36
Ope22 <opemipoi@outlook.com>
2022-06-04 04:49:44 -0700

    3 print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(c + i);
+	}
+	for (i = 0; i < 26; i++)
+	{
+		putchar((c + i) - 32);
+	}
+	putchar(10);
+	return (0);
+}
