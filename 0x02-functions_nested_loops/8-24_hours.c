35f0a7243a6427162c169e254b44d48e21654dd0
Ope22 <opemipoi@outlook.com>
2022-06-08 20:58:03 -0700

    c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,13 +14,13 @@ void jack_bauer(void)
 	{
 		for (b = 48; b <= 57; b++)
 		{
-			if (a == 50 && b ==52)
+			if (a == 50 && b == 52)
 			{
 				break;
 			}
-			for (c =48; c <= 53; c++)
+			for (c = 48; c <= 53; c++)
 			{
-				for (d = 48; d <=57; d++)
+				for (d = 48; d <= 57; d++)
 				{
 					_putchar(a);
 					_putchar(b);
