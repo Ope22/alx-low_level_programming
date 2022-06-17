68d1cba5e63cc90b50c6b1745db83b01830d0b5f
Ope22 <opemipoi@outlook.com>
2022-06-16 21:36:49 -0700

    leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * leet - encodes a string into 1337
- * @s; input string 
- * Return: the pointer to dest.
+ *  leet - encodes a string into 1337
+ *   @s; input string 
+ *   Return: the pointer to dest.
  */
 char *leet(char *s)
 {
