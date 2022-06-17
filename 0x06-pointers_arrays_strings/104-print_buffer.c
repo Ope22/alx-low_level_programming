34812a8a9e36ae65fa9eb7e0336b965f8b72c877
Ope22 <opemipoi@outlook.com>
2022-06-17 09:21:44 -0700

    print

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * isPrintableASCII - deternibes if n is a printable ASCII char
+ * isPrintableASCII - determines if n is a printable ASCII char
  * @n: integer
  * Return: 1 if true, 0 if false
  */
