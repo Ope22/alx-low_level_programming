3ccda951f44f9c4c00502065de4a11a70aacfd8d
Ope22 <opemipoi@outlook.com>
2022-06-17 09:14:01 -0700

    infinite

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -33,6 +33,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		if (i >= 0)
 			f = n1[i] - '0';
 		else
+			f = 0;
+		if (j >= 0)
+			s = n2[j] - '0';
+		else
 			s = 0;
 		r[k] = (f + s + d) % 10 + '0';
 		d = (f + s + d) / 10;
