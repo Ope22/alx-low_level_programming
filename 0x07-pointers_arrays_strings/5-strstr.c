76e6cc4a373908427c20edb7291cae93b9bdf150
Ope22 <opemipoi@outlook.com>
2022-06-20 16:04:11 -0700

    pointer

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -7,11 +7,11 @@
  * @haystack: This is the C string to be scanned
  * @needle: second parameter
  * Return: a pointer to the first occurrence of
- * needle in haystack or a null pointer if 
+ * needle in haystack or a null pointer if
  * needle is not part of haystack
  */
 
-char *_strstr (char *haystack, char *needle)
+char *_strstr(char *haystack, char *needle)
 {
 	return (strstr(haystack, needle));
 }
