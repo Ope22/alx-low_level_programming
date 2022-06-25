2cae07c5515137f92d2de05564fbc46a669e8b5b
Ope22 <104791614+Ope22@users.noreply.github.com>
2022-06-25 05:12:01 +0100

    Update 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -  a program that prints all arguments it receives.
+ * @argc: this is the argument count
+ * @argv: this is the argument vector
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int a;
+
+	for (a = 0; a < argc; a++)
+	{
+		printf("%s\n", argv[a]);
+	}
+	return (0);
+}
