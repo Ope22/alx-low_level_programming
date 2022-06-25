2fe45c63a7d9ee8fb1d7e06d5fd02631137de619
Ope22 <104791614+Ope22@users.noreply.github.com>
2022-06-25 06:05:54 +0100

    Update 1-isdigit.c

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * _isdigit - check digit.
+ *
+ * @c: integer
+ *
+ * Return: 1 succes
+ *
+ */
+
+int _isdigit(int c)
+
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+		else
+		{
+			return (0);
+		}
+}
