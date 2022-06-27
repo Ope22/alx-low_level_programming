19fe94362aa91e94b09e4c9d3dab16a3468a1e7d
Ope22 <opemipoi@outlook.com>
2022-06-27 09:05:05 -0700

    static

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -2,14 +2,14 @@
 #include <string.h>
 
 /**
- *  * _memset - a function that fills memory with a constant byte.
- *   * @s: pointer
- *    * @b: constant bytes
- *     * @n: bytes
- *      * Return: a pointer to the memory area s
- *       */
+ * * _memset - a function that fills memory with a constant byte.
+ * * @s: pointer
+ * * @b: constant bytes
+ * * @n: bytes
+ * * Return: a pointer to the memory area s
+ **/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-		return (memset(s, b, n));
+	return (memset(s, b, n));
 }
