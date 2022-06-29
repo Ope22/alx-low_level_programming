6eedbda3e78b21a2d36542dd769496eefc9646aa
Opemipo <104791614+Ope22@users.noreply.github.com>
2022-06-29 13:11:30 +0100

    Update 100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,43 +1,49 @@
 #include "main.h"
 #include <stdlib.h>
-
-
+#include <string.h>
+#include <stdio.h>
 /**
- * argstostr - concatenates all the arguments of your program.
- * @c: number of arguments
- * @av: double pointer to arguments
- *
- * Return:pointer to new string, or NULL if error
- **/
+ * argstostr - a function that concatenates all the arguments of your program.
+ * @ac: first parameter
+ * @av: second parameter
+ * Return: NULL or pointer to the array.
+ */
+
 char *argstostr(int ac, char **av)
 {
-	int i, j, k = 0, n = 0;
-	char *s;
+	char *aout;
+	int c, i, j, ia;
 
-	if (ac <= 0 || av == NULL)
+	if (ac == 0)
 		return (NULL);
-	for (i = 0; i < ac; i++)
+	for (c = i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j]; j++)
-			n++;
-		n++;
+		if (av[i] == NULL)
+			return (NULL);
+		for (j = 0; av[i][j] != '\0'; j++)
+			c++;
+		c++;
 	}
-	n++;
 
-	s = malloc(n * sizeof(char));
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; i < ac; i++)
+	aout = malloc((c + 1) * sizeof(char));
+
+	if (aout == NULL)
 	{
-		for (j = 0; av[i][j]; j++)
+		free(aout);
+		return (NULL);
+	}
+	for (i = j = ia = 0; ia < c; j++, ia++)
+	{
+		if (av[i][j] == '\0')
 		{
-			s[k] = av[i][j];
-			k++;
+			aout[ia] = '\n';
+			i++;
+			ia++;
+			j = 0;
 		}
-		s[k] = '\n';
-		k++;
-
+		if (ia < c - 1)
+			aout[ia] = av[i][j];
 	}
-	s[k] = '\0';
-	return (s);
+	aout[ia] = '\0';
+	return (aout);
 }
