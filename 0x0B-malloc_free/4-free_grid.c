9e199b436cd63e5f0275a7e1c834166e697ae9aa
Ope22 <opemipoi@outlook.com>
2022-06-28 17:23:44 -0700

    malloc

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -16,6 +16,6 @@ void free_grid(int **grid, int height)
 	if (grid == NULL || height <= 0)
 		return;
 	for (i = 0; i < height; i++)
-		free(grid[i];
-				free(grid);
+		free(grid[i]);
+	free(grid);
 }
