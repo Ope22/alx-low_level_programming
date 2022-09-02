03089afcc782e9d345a27d30a0d7e7a10cc176e5
Opemipo <104791614+Ope22@users.noreply.github.com>
2022-09-02 02:05:29 +0100

    Update lists.h

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,7 +1,7 @@
-#ifndef _LISTS_
-#define _LISTS_
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef _LISTS_H_
+#define _LISTS_H_
+
+#include <stddef.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -14,9 +14,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
@@ -29,4 +29,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif
+#endif /* _LISTS_H_ */
