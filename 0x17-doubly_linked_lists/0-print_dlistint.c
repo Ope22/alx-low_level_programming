bd23c0b8a3c95344c436ec7667e5489bf3fc2402
Ope22 <opemipoi@outlook.com>
2022-09-01 18:15:46 -0700

    doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,48 @@
 #include "lists.h"
 #include <stdio.h>
+ *spalindrome(n):
+	
+	    string = str(n)
+       
+    return string == string[::-1]
 
-/**
- * print_dlistint - print all the elements of a dlistint_t list
+
+
+array = []
+
+for i in range(999, 99, -1):
+	
+	    for j in range(i, 99, -1):
+		    
+		            if ispalindrome(i * j):
+				    
+				                array.append(i * j)
+	
+	print(max(array))print_dlistint - print all the elements of a dlistint_t list
  * @h: pointer to the start of the linked list
  *
  * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
-{
-	size_t i;
+spalindrome(n):
+       
+    string = str(n)
+
+    return string == string[::-1]
+
+
+
+array = []
+
+for i in range(999, 99, -1):
+	
+	    for j in range(i, 99, -1):
+		    
+		            if ispalindrome(i * j):
+				    
+				                array.append(i * j)
+	
+	print(max(array))i;
 
 	for (i = 0; h != NULL; i++)
 	{
