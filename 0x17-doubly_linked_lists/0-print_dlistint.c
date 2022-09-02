06ced119635004e366194a089582a2cc34534595
Ope22 <opemipoi@outlook.com>
2022-09-01 18:49:56 -0700

    doubly linked lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -40,7 +40,55 @@ for i in range(999, 99, -1):
 		    
 		            if ispalindrome(i * j):
 				    
-				                array.append(i * j)
+				                array.append(nclude "lists.h"
+								
+								
+								
+								/**
+								 *
+								 *  *print_dlistint - print the elements in the doubly link list
+								 *
+								 *   *@h: double linked list
+								 *
+								 *    * Return: number of elements inside the doubly linked list
+								 *
+								 *     */
+								
+								
+								
+								size_t print_dlistint(const dlistint_t *h)
+								
+								{
+								
+									size_t node = 0;
+									
+									
+									
+										if (h == NULL)
+											
+													return (node);
+										
+											/*find the first element*/
+										
+											while (h->prev != NULL)
+												
+														h = h->prev;
+											
+												while (h)
+													
+														{
+															
+																	printf("%d\n", h->n);
+																	
+																			h = h->next;
+																			
+																					node += 1;
+																					
+																						}
+												
+													return (node);
+													
+								}i * j)
 	
 	print(max(array))i;
 
