ac143f1486d9a1318e869b42ad78c8825f79f5bf
Opemipo <104791614+Ope22@users.noreply.github.com>
2022-09-02 02:54:14 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -6,71 +6,6 @@
  * Return: number of elements inside the doubly linked list
  */
 
-<<<<<<< HEAD
-
-
-array = []
-
-for i in range(999, 99, -1):
-	
-	    for j in range(i, 99, -1):
-		    
-		            if ispalindrome(i * j):
-				    
-				                array.append(nclude "lists.h"
-								
-								
-								
-								/**
-								 *
-								 *  *print_dlistint - print the elements in the doubly link list
-								 *
-								 *   *@h: double linked list
-								 *
-								 *    * Return: number of elements inside the doubly linked list
-								 *
-								 *     */
-								
-								
-								
-								size_t print_dlistint(const dlistint_t *h)
-								
-								{
-								
-									size_t node = 0;
-									
-									
-									
-										if (h == NULL)
-											
-													return (node);
-										
-											/*find the first element*/
-										
-											while (h->prev != NULL)
-												
-														h = h->prev;
-											
-												while (h)
-													
-														{
-															
-																	printf("%d\n", h->n);
-																	
-																			h = h->next;
-																			
-																					node += 1;
-																					
-																						}
-												
-													return (node);
-													
-								}i * j)
-	
-	print(max(array))i;
-
-	for (i = 0; h != NULL; i++)
-=======
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t node = 0;
@@ -81,7 +16,6 @@ size_t print_dlistint(const dlistint_t *h)
 	while (h->prev != NULL)
 		h = h->prev;
 	while (h)
->>>>>>> d7a1450d99bc82690bcb096a499ac19bfaf934fa
 	{
 		printf("%d\n", h->n);
 		h = h->next;
