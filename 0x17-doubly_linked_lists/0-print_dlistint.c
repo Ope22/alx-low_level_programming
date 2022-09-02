3420cd632a4ff7765c5a215d71d27f151afdef93
Opemipo <104791614+Ope22@users.noreply.github.com>
2022-09-02 02:46:48 +0100

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,53 +1,25 @@
 #include "lists.h"
-#include <stdio.h>
- *spalindrome(n):
-	
-	    string = str(n)
-       
-    return string == string[::-1]
 
-
-
-array = []
-
-for i in range(999, 99, -1):
-	
-	    for j in range(i, 99, -1):
-		    
-		            if ispalindrome(i * j):
-				    
-				                array.append(i * j)
-	
-	print(max(array))print_dlistint - print all the elements of a dlistint_t list
- * @h: pointer to the start of the linked list
- *
- * Return: number of nodes
+/**
+ *print_dlistint - print the elements in the doubly link list
+ *@h: double linked list
+ * Return: number of elements inside the doubly linked list
  */
+
 size_t print_dlistint(const dlistint_t *h)
-spalindrome(n):
-       
-    string = str(n)
+{
+	size_t node = 0;
 
-    return string == string[::-1]
-
-
-
-array = []
-
-for i in range(999, 99, -1):
-	
-	    for j in range(i, 99, -1):
-		    
-		            if ispalindrome(i * j):
-				    
-				                array.append(i * j)
-	
-	print(max(array))i;
-
-	for (i = 0; h != NULL; i++)
+	if (h == NULL)
+		return (node);
+	/*find the first element*/
+	while (h->prev != NULL)
+		h = h->prev;
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		node += 1;
 	}
-	return (i);
+	return (node);
 }
